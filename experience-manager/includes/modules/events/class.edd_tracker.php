cc243488e0606c057359f0e32f3ddb76105512bd
Thorsten Marx ㋡ <thmarx@gmx.net>
2019-12-01 20:21:36 +0100

    fix namespace

--- experience-manager/includes/modules/events/class.edd_tracker.php
+++ experience-manager/includes/modules/events/class.edd_tracker.php
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace TMA\ExperienceManager;
+namespace TMA\ExperienceManager\Events;
 
 /**
  * Tracking of WooCommerce events.
